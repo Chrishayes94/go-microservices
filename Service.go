f889ac5f1b98ba54ee79d4d2fdd79718248683ea
chris.hayes <chris.hayes@norsk-global.com>
2020-05-22 23:58:41 +0100

    Update Service.go

--- Service.go
+++ Service.go
@@ -19,5 +19,6 @@ func (s *Service) New() (router *mux.Router) {
 	r.HandleFunc("/book", s.handler.Book)
 	r.HandleFunc("/cancel", s.handler.Cancel)
 	r.HandleFunc("/label", s.handler.Label)
+	r.HandleFunc("/configuration", s.handler.Configuration)
 	return r
 }
