9038cd4667323a336f18db38fd143ca44555a191
chris.hayes <chris.hayes@norsk-global.com>
2020-05-22 15:25:47 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,11 +3,3 @@ package main
 import (
 	"go-microservices/handlers"
 )
-
-var asendiaHandler = new(handlers.AsendiaHandler)
-
-func main() {
-	//service := router2.New(":8080", asendiaHandler)
-	//router := service.New()
-	//log.Fatal(http.ListenAndServe(service.ServerAddress, router))
-}
