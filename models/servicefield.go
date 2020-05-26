dcff89ee745f6760521c912ada094249881e5d46
chris.hayes <chris.hayes@norsk-global.com>
2020-05-26 14:23:24 +0100

    Update servicefield.go

--- models/servicefield.go
+++ models/servicefield.go
@@ -1,9 +1,6 @@
 package models
 
-import "github.com/chrishayes94/go-microservices/cfg"
-
 type ServiceField struct {
 	Name  string        `json:"Name"`
-	Type  cfg.FieldType `json:"Type"`
 	Value string        `json:"Value"`
 }
